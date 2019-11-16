f2458652e24810c2258da661bc71661a89ec2f71
Berrin Nur <berrin05nur@gmail.com>
2019-11-16 18:09:30 +0100

    homework 1 completed

---
+++ week1/isim.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+
+int main()
+
+{
+    printf("Name   :Asli Nur Simsek \n");
+    printf("DOB    :November 13,2005\n");
+    printf("Mobile :+90240940       \n");
+}
---
+++ week1/merhabadunya.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main()
+{
+    printf("Hello, World!\n");
+    printf("Hallo,Wereld!\n");
+    printf("Olá Mundo!\n");
+    printf("Merhaba,Dunya!\n");
+    printf("Sveika pasaule!\n");
+    printf("Molo Lizwe!\n");
+    printf("Hai dunia!\n");
+    printf("Saluton MondoS!\n");
+    printf("Salamu, Dunia!\n");
+    printf("Ciao Mondo!\n");
+}
