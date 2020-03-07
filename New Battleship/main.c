3b47cfa1e16c7530a53451312f3392aa50e91483
Berrin Nur <berrin05nur@gmail.com>
2020-03-07 16:20:56 +0100

    added element control

--- New Battleship/main.c
+++ New Battleship/main.c
@@ -1,8 +1,13 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include "c_fun.c"
+
 char gameTable[10][10];
 char screenTable[10][10];
+int row, col;
+char elementTypes[9] = {'A', 'W', 'E', 'U', 'C', 'O', 'O', 'O', 'O'};
+int elementValues[9] = {5, 3, 3, 2, 2, 1, 1, 1, 1};
+int elementPlaces[5];
 
 void clear()
 {
@@ -16,43 +21,150 @@ void prepTables()
         for (int j = 0; j < 10; j++)
         {
             char c = '0';
-            if (i == 0)
+            if(i == 0)
             {
                 c += j;
             }
-            else if (j == 0)
+            else if(j == 0)
             {
                 c += i;
             }
             else
             {
-                c + '0';
+                c = ' ';
             }
+            gameTable[i][j] = c;
+            screenTable[i][j] = c;
         }
     }
-
 }
+
 void printTable()
+{
+    clear();
+    for (int i = 0; i < 10; i++)
+    {
+        for (int j = 0; j < 10; j++)
+        {
+            printf("%c ", screenTable[i][j]);
+        }
+        printf("\n");
+    }
+}
+
+void placeElement(int r, int c, char propType)
+{
+    screenTable[r][c] = propType;
+}
+
+void placeProp(char propType, int numberOfElements)
+{
+    for (int i = 0; i < numberOfElements; i++)
+    {
+        placeElement(elementPlaces[i]/10, elementPlaces[i]%10, propType);
+    }
+}
+
+char getUserInput()
+{
+    char c = get_ch();
+    if(c >= 'A' && c <= 'Z')
+    {
+        c = c + 32;
+    }
+    return c;
+}
+
+void copyTable()
 {
     for (int i = 0; i < 10; i++)
     {
         for (int j = 0; j < 10; j++)
         {
-            printf("%c", screenTable[i][j]);
+            screenTable[i][j] = gameTable[i][j];
         }
-        printf("\n");       
     }
-    
 }
-char getUserInput()
+
+void prepValues(char elementType)
 {
-    char c;
-
-    return c;
+    switch (elementType)
+    {
+        case 'A':
+            elementPlaces[0] = row * 10 + col;
+            elementPlaces[1] = row * 10 + col + 2;
+            elementPlaces[2] = (row + 1) * 10 + col + 1;
+            elementPlaces[3] = (row + 2) * 10 + col;
+            elementPlaces[4] = (row + 2) * 10 + col + 2;
+            break;
+        case 'W':
+            elementPlaces[0] = row * 10 + col;
+            elementPlaces[1] = (row + 1) * 10 + col;
+            elementPlaces[2] = (row + 2) * 10 + col;
+            break;
+        case 'E':
+            elementPlaces[0] = row * 10 + col;
+            elementPlaces[1] = row * 10 + col + 1;
+            elementPlaces[2] = row * 10 + col + 2;
+            break;
+        case 'U':
+            elementPlaces[0] = row * 10 + col;
+            elementPlaces[1] = (row + 1) * 10 + col;
+            break;
+        case 'C': 
+            elementPlaces[0] = row * 10 + col;
+            elementPlaces[1] = row * 10 + col + 1;
+            break;
+        case 'O':
+            elementPlaces[0] = row * 10 + col;
+            break;
+    }
 }
 
-int main()
+void start()
 {
     prepTables();
     printTable();
+    for(int i = 0; i < 9; i++)
+    {
+        char elementType = elementTypes[i];
+        int elementValue = elementValues[i];
+        row = 5;
+        col = 5;
+        char c;
+        while(1)
+        {
+            prepValues(elementType);
+            placeProp(elementType, elementValue);
+            printTable();
+            c = getUserInput();
+            switch (c)
+            {
+                case 'w':
+                    row--;
+                    break;
+                case 'a':
+                    col--;
+                    break;
+                case 's':
+                    row++;
+                    break;
+                case 'd':
+                    col++;
+                    break;
+            }
+            if(c == '\n')
+            {
+                // Put element to game table.
+                break;
+            }
+            copyTable();
+        }
+    }
+}
+
+
+int main()
+{
+    start();
 }
